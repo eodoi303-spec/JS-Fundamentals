6c44cc45a0c172ae431dbd2560ef91ae2eab0a2e
Ethel-Odoi <eodoi303@email.com>
2025-11-08 06:42:25 +0000

    1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+// 1-multi_languages.js
+
+const first = "C is fun";
+const second = "Python is cool";
+const third = "JavaScript is amazing";
+
+console.log(first);
+console.log(second);
+console.log(third);
